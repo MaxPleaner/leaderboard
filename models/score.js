eba6065aa44bf01664450fce07e58579b0b0b7f1
max pleaner <maxpleaner@gmaeil.com>
2017-07-27 10:06:17 -0700

    stop using sequelize ... too much boiler

--- models/score.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, data_types) ->
-  sequelize.define "Score", {
-    user: DataTypes.STRING
-    game:DataTypes.STRING
-    value: DataTypes.Integer
-  }, {
-    classMethods:
-      associate: (models) ->
-  }
